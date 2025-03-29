a150c225d26ad948f988d8d12aad62192f2f8991
Zepth <taitanerenwearenumber@gmail.com>
2025-03-29 11:52:10 +0700

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -1,3 +1,103 @@
+--// Cache
+
+local getgenv, getnamecallmethod, hookmetamethod, hookfunction, newcclosure, checkcaller, lower, gsub, match = getgenv, getnamecallmethod, hookmetamethod, hookfunction, newcclosure, checkcaller, string.lower, string.gsub, string.match
+
+--// Loaded check
+
+if getgenv().ED_AntiKick then
+	return
+end
+
+--// Variables
+
+local cloneref = cloneref or function(...) 
+	return ...
+end
+
+local clonefunction = clonefunction or function(...)
+	return ...
+end
+
+local Players, LocalPlayer, StarterGui = cloneref(game:GetService("Players")), cloneref(game:GetService("Players").LocalPlayer), cloneref(game:GetService("StarterGui"))
+
+local SetCore = clonefunction(StarterGui.SetCore)
+--local GetDebugId = clonefunction(game.GetDebugId)
+local FindFirstChild = clonefunction(game.FindFirstChild)
+
+local CompareInstances = (CompareInstances and function(Instance1, Instance2)
+		if typeof(Instance1) == "Instance" and typeof(Instance2) == "Instance" then
+			return CompareInstances(Instance1, Instance2)
+		end
+	end)
+or
+function(Instance1, Instance2)
+	return (typeof(Instance1) == "Instance" and typeof(Instance2) == "Instance")-- and GetDebugId(Instance1) == GetDebugId(Instance2)
+end
+
+local CanCastToSTDString = function(...)
+	return pcall(FindFirstChild, game, ...)
+end
+
+--// Global Variables
+
+getgenv().ED_AntiKick = {
+	Enabled = true, -- Set to false if you want to disable the Anti-Kick.
+	SendNotifications = true, -- Set to true if you want to get notified for every event.
+	CheckCaller = true -- Set to true if you want to disable kicking by other user executed scripts.
+}
+
+--// Main
+
+local OldNamecall; OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
+	local self, message = ...
+	local method = getnamecallmethod()
+	
+	if ((getgenv().ED_AntiKick.CheckCaller and not checkcaller()) or true) and CompareInstances(self, LocalPlayer) and gsub(method, "^%l", string.upper) == "Kick" and ED_AntiKick.Enabled then
+		if CanCastToSTDString(message) then
+			if getgenv().ED_AntiKick.SendNotifications then
+				SetCore(StarterGui, "SendNotification", {
+					Title = "Exunys Developer - Anti-Kick",
+					Text = "Successfully intercepted an attempted kick.",
+					Icon = "rbxassetid://6238540373",
+					Duration = 2
+				})
+			end
+
+			return
+		end
+	end
+
+	return OldNamecall(...)
+end))
+
+local OldFunction; OldFunction = hookfunction(LocalPlayer.Kick, function(...)
+	local self, Message = ...
+
+	if ((ED_AntiKick.CheckCaller and not checkcaller()) or true) and CompareInstances(self, LocalPlayer) and ED_AntiKick.Enabled then
+		if CanCastToSTDString(Message) then
+			if ED_AntiKick.SendNotifications then
+				SetCore(StarterGui, "SendNotification", {
+					Title = "Exunys Developer - Anti-Kick",
+					Text = "Successfully intercepted an attempted kick.",
+					Icon = "rbxassetid://6238540373",
+					Duration = 2
+				})
+			end
+
+			return
+		end
+	end
+end)
+
+if getgenv().ED_AntiKick.SendNotifications then
+	StarterGui:SetCore("SendNotification", {
+		Title = "Exunys Developer - Anti-Kick",
+		Text = "Anti-Kick script loaded!",
+		Icon = "rbxassetid://6238537240",
+		Duration = 3
+	})
+end
+
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/zepthical/Vayfield/refs/heads/main/README.md'))()
  
  local Window = Rayfield:CreateWindow({
@@ -215,7 +315,7 @@ MainTab:CreateToggle({
 
 local function Reel()
     -- Wait a short delay to ensure everything is loaded
-    task.wait(0.1)
+    task.wait(0.2)
     
     -- Search through the player's GUI for the reel UI elements
     for _, v in pairs(LocalPlayer.PlayerGui:GetChildren()) do
@@ -242,6 +342,20 @@ local function Reel()
     end
 end
 
+local function Reset()
+    local Rod = Char and Char:FindFirstChildOfClass("Tool")
+    if Rod and Rod:FindFirstChild("events") and Rod.events:FindFirstChild("reset") then
+        task.wait(0.1)
+        Rod.events.reset:FireServer()
+        game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(Rod)
+        task.wait(0.1)
+        game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(Rod)
+
+    else
+        warn("Rod or reset event not found!")
+    end
+end
+
 MainTab:CreateToggle({
     Name = "Auto Reel",
     Flag = "AReel",
@@ -257,7 +371,8 @@ MainTab:CreateToggle({
                     if Rod.values.bite.Value == true then 
                         Reel() -- Reel once when bite is detected
                         task.wait(3)
-                        Reel()                    
+                        Reel()    
+                        Reset()
                         repeat task.wait(0.1) until Rod.values.bite.Value == false
                     end
                 else
@@ -297,21 +412,8 @@ MainTab:CreateToggle({
     end
  })
  
- local Divider = MainTab:CreateDivider()
+local Divider = MainTab:CreateDivider()
 
- local function Reset()
-    local Rod = Char and Char:FindFirstChildOfClass("Tool")
-    if Rod and Rod:FindFirstChild("events") and Rod.events:FindFirstChild("reset") then
-        task.wait(0.1)
-        Rod.events.reset:FireServer()
-        game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(Rod)
-        task.wait(0.1)
-        game:GetService("ReplicatedStorage").packages.Net:FindFirstChild("RE/Backpack/Equip"):FireServer(Rod)
-
-    else
-        warn("Rod or reset event not found!")
-    end
-end
 
 MainTab:CreateToggle({
     Name = "Instant Reel[WIP]",
@@ -345,6 +447,7 @@ MainTab:CreateToggle({
                                         Reel()
                                         task.wait(3)
                                         Reel()
+                                        Reset()
                                     end
                                 end
                             end
